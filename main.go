b3972cd1aa43ee14d25783e1ab8b54642c52a895
Jake Bailey <jacob.b.bailey@gmail.com>
2017-07-05 22:02:38 -0700

    switch parsing library

--- main.go
+++ main.go
@@ -1,38 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 
-	"github.com/namsral/flag"
+	"github.com/alexflint/go-arg"
 )
 
+var args = struct {
+	Port   uint   `arg:"env"`
+	URL    string `arg:"required,env:REDIR_URL,help:url to redirect all requests to"`
+	Scheme string `arg:"env:REDIR_SCHEME,help:scheme to use if not specified in url"`
+}{
+	Port:   5000,
+	Scheme: "http",
+}
+
 func main() {
-	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "REDIR", flag.ExitOnError)
+	p := arg.MustParse(&args)
 
-	ur := fs.String("url", "", "url to redirect all requests to")
-	scheme := fs.String("scheme", "http", "scheme to use if not specified in url")
-	addr := fs.String("addr", ":5000", "address to run the server on")
-
-	fs.Parse(os.Args[1:])
-
-	if *ur == "" {
-		log.Fatal("url must be provided")
-	}
-
-	u, err := url.Parse(*ur)
-
+	u, err := url.Parse(args.URL)
 	if err != nil {
-		log.Fatal(err)
+		p.Fail("error parsing URL: " + err.Error())
 	}
 
 	if u.Scheme == "" {
-		u.Scheme = *scheme
+		u.Scheme = args.URL
 	}
 
-	log.Printf("redirecting all traffic to %v", u)
+	log.Println("redirecting all traffic to", u)
 
-	log.Fatal(http.ListenAndServe(*addr, http.RedirectHandler(u.String(), http.StatusSeeOther)))
+	addr := fmt.Sprintf(":%v", args.Port)
+	log.Println("starting server at", addr)
+
+	handler := http.RedirectHandler(u.String(), http.StatusSeeOther)
+	log.Fatal(http.ListenAndServe(addr, handler))
 }
